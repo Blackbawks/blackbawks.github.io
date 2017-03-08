223ae59e9b86db03919fa26e1dd1945b6871d7a4
grwhumphries <grant@leucorhoa.home>
2017-03-08 17:32:07 +0000

    add new blog post March 8

--- js/blogs.js
+++ js/blogs.js
@@ -1,5 +1,13 @@
 
 $( document ).ready( function() {
+
+	$(".getblog").click(function () {
+        	var addressValue = $(this).attr("href");
+        	$.get(addressValue,function(data) {
+			$( "#mostrecent" ).html( data );
+		});
+    	});
+
 	$.get('/blogs/2017/Mesoscale-data-science.html', function(data) {
 		$( "#mostrecent" ).html( data );
 	});
